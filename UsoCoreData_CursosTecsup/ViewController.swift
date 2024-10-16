42ca1da9a0d385b7b661c9cf59f0a68846ec9535
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-10-16 11:08:39 -0500

    Initial Commit

---
+++ UsoCoreData_CursosTecsup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsoCoreData_CursosTecsup
+//
+//  Created by Mac05 on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
